1ae7b1e6bd74ffdafea03d1d764859c5521239be
selvamani <selvamani0394@gmail.com>
2021-10-21 20:48:12 -0700

    mytask

---
+++ src/org/emp/Sample.java
@@ -0,0 +1,16 @@
+package org.emp;
+
+public class Sample {
+	public void empid() {
+System.out.println("employee id is 12345");
+	}
+	public void empnum() {
+System.out.println("employee number is 123");
+	}
+	public static void main(String[] args) {
+		Sample s = new Sample();
+		s.empid();
+		s.empnum();
+	}
+
+}
